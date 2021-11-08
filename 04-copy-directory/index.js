0a569f275631fbce0046098b4872196142d246ff
Alexei Lyashenko <lyashenko1alexei@gmail.com>
2021-11-08 18:58:57 +0200

    feat: add 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs'),
+      path = require('path'),
+      fsPromises = fs.promises;
 
+async function copyDirectory() {
+    await fsPromises.rm(
+        path.join(__dirname, 'files-copy'),
+        { recursive:true, force: true }
+    );
+    
+    await fsPromises.mkdir(
+        path.join(__dirname, 'files-copy'),
+        { recursive: true },
+        err => {
+            if(err) throw err;
+        })
+
+    const files = await fsPromises.readdir(path.join(__dirname, 'files'));
+    
+    for await (const file of files) {
+        console.log(file);
+        fsPromises.copyFile(
+            path.join(__dirname, 'files', file),
+            path.join(__dirname, 'files-copy', file)
+        )
+    }    
+}
+copyDirectory()
